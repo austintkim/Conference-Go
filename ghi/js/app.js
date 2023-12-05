7a69075e232a6570afb208d7f8f8e0aa534e852b
Austin Kim <mambafinisher@gmail.com>
2023-12-05 14:41:27 -0800

    Card Template

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,3 +1,16 @@
+function createCard(name, description, pictureUrl) {
+  return `
+  <div class="col-6 col-sm-4">
+    <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
+      <img src="${pictureUrl}" class="card-img-top">
+      <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">${description}</p>
+      </div>
+    </div>
+  `;
+}
+
 window.addEventListener('DOMContentLoaded', async () => {
 
     const url = 'http://localhost:8000/api/conferences/';
@@ -10,19 +23,18 @@ window.addEventListener('DOMContentLoaded', async () => {
       } else {
         const data = await response.json();
 
-        const conference = data.conferences[0];
-        const nameTag = document.querySelector('.card-title');
-        nameTag.innerHTML = conference.name;
-
-        const detailUrl = `http://localhost:8000${conference.href}`;
-        const detailResponse = await fetch(detailUrl);
-        if (detailResponse.ok) {
-          const details = await detailResponse.json();
-          const detailTag = document.querySelector('.card-text');
-          detailTag.innerHTML = details.conference.description;
-
-          const imageTag = document.querySelector('.card-img-top')
-          imageTag.src = details.conference.location.picture_url;
+        for (let conference of data.conferences) {
+          const detailUrl = `http://localhost:8000${conference.href}`;
+          const detailResponse = await fetch(detailUrl);
+          if (detailResponse.ok) {
+            const details = await detailResponse.json();
+            const title = details.conference.name;
+            const description = details.conference.description;
+            const pictureUrl = details.conference.location.picture_url;
+            const html = createCard(title, description, pictureUrl);
+            const column = document.querySelector('.row');
+            column.innerHTML += html;
+          }
         }
 
       }
