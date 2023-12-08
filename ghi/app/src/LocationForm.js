63246494fbaa1cd7b983bcc358d834104ec35f96
Austin Kim <mambafinisher@gmail.com>
2023-12-08 13:11:07 -0800

    Submitting The Form

--- ghi/app/src/LocationForm.js
+++ ghi/app/src/LocationForm.js
@@ -36,6 +36,36 @@ function LocationForm(props) {
     setState(value);
   }
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    //create an empty JSON object
+    const data = {};
+    data.room_count = roomCount;
+    data.name = name;
+    data.city = city;
+    data.state = state;
+
+    const locationUrl = 'http://localhost:8000/api/locations/';
+    const fetchConfig = {
+      method: "post",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const response = await fetch(locationUrl, fetchConfig);
+    if (response.ok) {
+      const newLocation = await response.json();
+
+      setName('');
+      setRoomCount('');
+      setCity('');
+      setState('');
+    }
+  }
+
   const fetchData = async () => {
     const url = 'http://localhost:8000/api/states/';
 
@@ -47,6 +77,7 @@ function LocationForm(props) {
     }
   }
 
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -56,21 +87,21 @@ function LocationForm(props) {
         <div className="offset-3 col-6">
           <div className="shadow p-4 mt-4">
             <h1>Create a new location</h1>
-            <form id="create-location-form">
+            <form onSubmit={handleSubmit} id="create-location-form">
               <div className="form-floating mb-3">
-                <input onChange={handleNameChange} placeholder="Name" required type="text" name="name" id="name" className="form-control"/>
+                <input onChange={handleNameChange} placeholder="Name" required type="text" name="name" id="name" className="form-control" value={name}/>
                 <label htmlFor="name">Name</label>
               </div>
               <div className="form-floating mb-3">
-                <input onChange={handleRoomCountChange} placeholder="Room count" required type="number" name="room_count" id="room_count" className="form-control"/>
+                <input onChange={handleRoomCountChange} placeholder="Room count" required type="number" name="room_count" id="room_count" className="form-control" value={roomCount}/>
                 <label htmlFor="room_count">Room count</label>
               </div>
               <div className="form-floating mb-3">
-                <input onChange={handleCityChange} placeholder="City" required type="text" name="city" id="city" className="form-control"/>
+                <input onChange={handleCityChange} placeholder="City" required type="text" name="city" id="city" className="form-control" value={city}/>
                 <label htmlFor="city">City</label>
               </div>
               <div className="mb-3">
-                <select onChange={handleStateChange} required name="state" id="state" className="form-select">
+                <select onChange={handleStateChange} required name="state" id="state" className="form-select" value={state}>
                   <option value="">Choose a state</option>
                   {states.map(state => {
                     return (
