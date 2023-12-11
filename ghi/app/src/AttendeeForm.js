6a5b13a585c9cfe618e155c40409a95f3ad59c7a
Austin Kim <mambafinisher@gmail.com>
2023-12-11 11:51:36 -0800

    Added Attendee form CSS and success messsage on valid form submission

--- ghi/app/src/AttendeeForm.js
+++ ghi/app/src/AttendeeForm.js
@@ -1,73 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
-function AttendeeForm(props) {
+function AttendeeForm() {
     const [conferences, setConferences] = useState([]);
 
     const [name, setName] = useState('');
 
     const [email, setEmail] = useState('');
 
-    const [companyName, setCompanyName] = useState('');
-
     const [conference, setConference] = useState('');
 
-
-    const handleNameChange = (event) => {
-        const value = event.target.value;
-        setName(value);
-      }
-
-      const handleEmailChange = (event) => {
-        const value = event.target.value;
-        setEmail(value);
-      }
-
-      const handleCompanyNameChange = (event) => {
-        const value = event.target.value;
-        setCompanyName(value);
-      }
-
-      const handleConferenceChange = (event) => {
-        const value = event.target.value;
-        setConference(value);
-      }
-
-      const handleSubmit = async (event) => {
-        event.preventDefault();
-            //create an empty JSON object
-        const data = {};
-        data.name = name;
-        data.email = email;
-        data.company_name = companyName;
-        data.conference = `/api/conferences/${conference}/`;
-        console.log(data);
-
-
-
-
-
-
-        const attendeeUrl = 'http://localhost:8001/api/attendees/';
-        const fetchConfig = {
-            method: "post",
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
-
-        const response = await fetch(attendeeUrl, fetchConfig);
-        if (response.ok) {
-            const newAttendee = await response.json();
-            console.log(newAttendee);
-
-        setName('');
-        setEmail('');
-        setCompanyName('');
-        setConference('');
-        }
-    }
-
+    const [hasSignedUp, setHasSignedUp] = useState(false);
 
     const fetchData = async () => {
         const url = 'http://localhost:8000/api/conferences/';
@@ -82,44 +24,125 @@ function AttendeeForm(props) {
 
     useEffect(() => {
         fetchData();
-      }, []);
+    }, []);
 
-    return(
-        <div className="container">
-        <div className="row">
-            <div className="offset-3 col-6">
-            <div className="shadow p-4 mt-4">
-                <h1>Add a new attendee</h1>
-                <form onSubmit={handleSubmit} id="create-attendee-form">
-                <div className="form-floating mb-3">
-                    <input onChange={handleNameChange} placeholder="Name" required type="text" name = "name" id="name" className="form-control"/>
-                    <label htmlFor="name">Name</label>
-                </div>
-                <div className="form-floating mb-3">
-                    <input onChange={handleEmailChange} placeholder="Email" required type="email" name = "email" id="email" className="form-control"/>
-                    <label htmlFor="email">Email</label>
-                </div>
-                <div className="form-floating mb-3">
-                    <input onChange={handleCompanyNameChange} placeholder="Ends" type="text" name = "company_name" id="company_name" className="form-control"/>
-                    <label htmlFor="company_name">Company Name</label>
-                </div>
-                <div className="mb-3">
-                    <select onChange={handleConferenceChange} required name = "conference" id="conference" className="form-select" value={conference}>
-                    <option value="">Choose a conference</option>
-                    {conferences.map(conference => {
-                        return(
-                            <option key={conference.id} value={conference.id}>
-                                {conference.name}
-                            </option>
-                        )
-                    })}
-                    </select>
-                </div>
-                <button className="btn btn-primary">Add</button>
-                </form>
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+          //create an empty JSON object
+      const data = {};
+      data.name = name;
+      data.email = email;
+      data.conference = `/api/conferences/${conference}/`;
+      console.log(data);
+
+
+      const attendeeUrl = 'http://localhost:8001/api/attendees/';
+      const fetchConfig = {
+          method: "post",
+          body: JSON.stringify(data),
+          headers: {
+              'Content-Type': 'application/json',
+          },
+      };
+
+      const response = await fetch(attendeeUrl, fetchConfig);
+      if (response.ok) {
+          const newAttendee = await response.json();
+          console.log(newAttendee);
+
+      setName('');
+      setEmail('');
+      setConference('');
+      setHasSignedUp(true);
+      }
+  }
+    const handleNameChange = (event) => {
+        const value = event.target.value;
+        setName(value);
+    }
+
+      const handleEmailChange = (event) => {
+        const value = event.target.value;
+        setEmail(value);
+      }
+
+      const handleConferenceChange = (event) => {
+        const value = event.target.value;
+        setConference(value);
+      }
+
+
+// CSS classes for rendering
+  let spinnerClasses = 'd-flex justify-content-center mb-3';
+  let dropdownClasses = 'form-select d-none';
+  if (conferences.length > 0) {
+    spinnerClasses = 'd-flex justify-content-center mb-3 d-none';
+    dropdownClasses = 'form-select';
+  }
+
+  let messageClasses = 'alert alert-success d-none mb-0';
+  let formClasses = '';
+  if (hasSignedUp) {
+    messageClasses = 'alert alert-success mb-0';
+    formClasses = 'd-none';
+  }
+
+    return (
+        <div className="my-5 container">
+          <div className="row">
+            <div className="col col-sm-auto">
+              <img width="300" className="bg-white rounded shadow d-block mx-auto mb-4" src="/logo.svg" />
             </div>
+            <div className="col">
+              <div className="card shadow">
+                <div className="card-body">
+                  <form className={formClasses} onSubmit={handleSubmit} id="create-attendee-form">
+                    <h1 className="card-title">It's Conference Time!</h1>
+                    <p className="mb-3">
+                      Please choose which conference
+                      you'd like to attend.
+                    </p>
+                    <div className={spinnerClasses} id="loading-conference-spinner">
+                      <div className="spinner-grow text-secondary" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                      </div>
+                    </div>
+                    <div className="mb-3">
+                      <select onChange={handleConferenceChange} name="conference" id="conference" className={dropdownClasses} required>
+                        <option value="">Choose a conference</option>
+                        {conferences.map(conference => {
+                          return (
+                            <option key={conference.id} value={conference.id}>{conference.name}</option>
+                          )
+                        })}
+                      </select>
+                    </div>
+                    <p className="mb-3">
+                      Now, tell us about yourself.
+                    </p>
+                    <div className="row">
+                      <div className="col">
+                        <div className="form-floating mb-3">
+                          <input onChange={handleNameChange} required placeholder="Your full name" type="text" id="name" name="name" className="form-control" />
+                          <label htmlFor="name">Your full name</label>
+                        </div>
+                      </div>
+                      <div className="col">
+                        <div className="form-floating mb-3">
+                          <input onChange={handleEmailChange} required placeholder="Your email address" type="email" id="email" name="email" className="form-control" />
+                          <label htmlFor="email">Your email address</label>
+                        </div>
+                      </div>
+                    </div>
+                    <button className="btn btn-lg btn-primary">I'm going!</button>
+                  </form>
+                  <div className={messageClasses} id="success-message">
+                    Congratulations! You're all signed up!
+                  </div>
+                </div>
+              </div>
             </div>
-        </div>
+          </div>
         </div>
     );
 }
