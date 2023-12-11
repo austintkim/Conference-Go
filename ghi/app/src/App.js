7253ec8b1a9f6a2fcb3c2ad0949aea18e9c3deb7
Austin Kim <mambafinisher@gmail.com>
2023-12-11 12:24:06 -0800

    Route the paths

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -4,16 +4,38 @@ import AttendeesList from './AttendeesList';
 import LocationForm from './LocationForm';
 import ConferenceForm from './ConferenceForm';
 import AttendeeForm from './AttendeeForm';
+import {
+  NavLink, Route, Routes, useNavigate
+} from "react-router-dom";
 
 function App(props) {
   return (
     <>
     <Nav />
     <div className="container">
+      <Routes>
+        <Route
+          path='/conferences/new'
+          element= {<ConferenceForm />}
+        ></Route>
+        <Route
+          path='/attendees/new'
+          element= {<AttendeeForm />}
+        ></Route>
+        <Route
+          path='/locations/new'
+          element= {<LocationForm />}
+        ></Route>
+        <Route
+          path='/attendees'
+          element= {<AttendeesList />}
+        ></Route>
+
+      </Routes>
       {/* <LocationForm /> */}
       {/* <AttendeesList attendees = {props.attendees}/> */}
       {/* <ConferenceForm /> */}
-      <AttendeeForm />
+      {/* <AttendeeForm /> */}
 
     </div>
     </>
--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -21,9 +22,9 @@ async function loadAttendees() {
   if (response.ok) {
     const data = await response.json();
     root.render(
-      <React.StrictMode>
+      <BrowserRouter>
         <App attendees={data.attendees} />
-      </React.StrictMode>
+      </BrowserRouter>
     );
   } else {
     console.error(response);
