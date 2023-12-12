5eef7efd87958b4f0a8b339ad1bf419cac4fddbd
Austin Kim <mambafinisher@gmail.com>
2023-12-12 13:45:34 -0800

    Refactoring Presentation Form

--- ghi/app/src/PresentationForm.js
+++ ghi/app/src/PresentationForm.js
@@ -1,23 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import {useNavigate} from "react-router-dom";
 
-function PresentationForm(props) {
+const initialData = {
+  presenter_name:"",
+  presenter_email:"",
+  company_name:"",
+  title:"",
+  synopsis:"",
+  conference:""
+}
+
+function PresentationForm() {
+    const navigate = useNavigate();
+
     const [conferences, setConferences] = useState([]);
 
-    const [name, setName] = useState('');
-
-    const [email, setEmail] = useState('');
-
-    const [companyName, setCompanyName] = useState('');
-
-    const [title, setTitle] = useState('');
-
-    const [synopsis, setSynopsis] = useState('');
-
-    const [conference, setConference] = useState('');
+    const [formData, setFormData] = useState(initialData);
 
     const fetchData = async () => {
         const url = 'http://localhost:8000/api/conferences/';
-
         const response = await fetch(url);
 
         if (response.ok) {
@@ -30,52 +31,21 @@ function PresentationForm(props) {
         fetchData();
       }, []);
 
-    const handleNameChange = (event) => {
-        const value = event.target.value;
-        setName(value);
-      }
-
-    const handleEmailChange = (event) => {
-        const value = event.target.value;
-        setEmail(value);
-      }
-
-    const handleCompanyNameChange = (event) => {
-        const value = event.target.value;
-        setCompanyName(value);
-      }
-
-    const handleTitleChange = (event) => {
-        const value = event.target.value;
-        setTitle(value);
-      }
-
-    const handleSynopsisChange = (event) => {
-        const value = event.target.value;
-        setSynopsis(value);
-      }
-
-    const handleConferenceChange = (event) => {
-        const value = event.target.value;
-        setConference(value);
-      }
+    const handleFormChange = (e) => {
+      setFormData({
+        ...formData,
+        [e.target.name]:e.target.value
+      })
+    }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-            //create an empty JSON object
-        const data = {};
-        data.presenter_name = name;
-        data.presenter_email = email;
-        data.company_name = companyName;
-        data.title = title;
-        data.synopsis = synopsis;
-        data.conference = conference;
-        console.log(data);
 
-        const conferenceUrl = `http://localhost:8000/api/conferences/${conference}/presentations/`;
+        const conferenceUrl = `http://localhost:8000/api/conferences/${formData.conference}/presentations/`;
+
         const fetchConfig = {
             method: "post",
-            body: JSON.stringify(data),
+            body: JSON.stringify(formData),
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -83,15 +53,8 @@ function PresentationForm(props) {
 
         const response = await fetch(conferenceUrl, fetchConfig);
         if (response.ok) {
-            const newPresentation = await response.json();
-            console.log(newPresentation);
-
-        setName('');
-        setEmail('');
-        setCompanyName('');
-        setTitle('');
-        setSynopsis('');
-        setConference('');
+            navigate("/");
+            setFormData(initialData);
         }
     }
 
@@ -103,27 +66,27 @@ function PresentationForm(props) {
               <h1>Create a new presentation</h1>
               <form onSubmit={handleSubmit} id="create-presentation-form">
                 <div className="form-floating mb-3">
-                  <input onChange={handleNameChange} placeholder="Presenter name" required type="text" name = "presenter_name" id="presenter_name" className="form-control" value={name}/>
+                  <input onChange={handleFormChange} placeholder="Presenter name" required type="text" name = "presenter_name" id="presenter_name" className="form-control" value={formData.name}/>
                   <label htmlFor="presenter_name">Presenter name</label>
                 </div>
                 <div className="form-floating mb-3">
-                  <input onChange={handleEmailChange} placeholder="Presenter email" required type="email" name = "presenter_email" id="presenter_email" className="form-control" value={email}/>
+                  <input onChange={handleFormChange} placeholder="Presenter email" required type="email" name = "presenter_email" id="presenter_email" className="form-control" value={formData.email}/>
                   <label htmlFor="presenter_email">Presenter email</label>
                 </div>
                 <div className="form-floating mb-3">
-                  <input onChange={handleCompanyNameChange} placeholder="Company name" required type="text" name = "company_name" id="company_name" className="form-control" value={companyName}/>
+                  <input onChange={handleFormChange} placeholder="Company name" required type="text" name = "company_name" id="company_name" className="form-control" value={formData.company_name}/>
                   <label htmlFor="company_name">Company name</label>
                 </div>
                 <div className="mb-3">
                 <div className="form-floating mb-3">
-                  <input onChange={handleTitleChange} placeholder="Title" required type="text" name = "title" id="title" className="form-control" value={title}/>
+                  <input onChange={handleFormChange} placeholder="Title" required type="text" name = "title" id="title" className="form-control" value={formData.title}/>
                   <label htmlFor="title">Title</label>
                 </div>
                   <label htmlFor="synopsis">Synopsis</label>
-                  <textarea onChange={handleSynopsisChange} name = "synopsis" id="synopsis" className="form-control" value={synopsis} rows="3"></textarea>
+                  <textarea onChange={handleFormChange} name = "synopsis" id="synopsis" className="form-control" value={formData.synopsis} rows="3"></textarea>
                 </div>
                 <div className="mb-3">
-                  <select onChange={handleConferenceChange}required name = "conference" id="conference" className="form-select" value={conference}>
+                  <select onChange={handleFormChange}required name = "conference" id="conference" className="form-select" value={formData.conference}>
                     <option value="">Choose a conference</option>
                     {conferences.map(conference => {
                         return (
